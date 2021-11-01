7b6eab2908f0a534242107ce11f63a0301a65a76
smhrd <smhrd@DESKTOP-9O7KL35>
2021-11-01 12:22:59 +0900

    상품 성분 표시 추가 -> Product(mapper, controller, service) ingreCountDTO 추가 (성분 표시용 임시 DTO)

---
+++ MySkin/src/main/java/kr/smhrd/domain/IngreCountDTO.java
@@ -0,0 +1,12 @@
+package kr.smhrd.domain;
+
+import lombok.Data;
+
+@Data
+public class IngreCountDTO {
+	private String o;
+	private String d;
+	private String s;
+	private String p;
+	private String w;
+}
